1f5fae334968bae025e3dcab11c1f17e41730be1
piyushchoudhary2001 <57358377+piyushchoudhary2001@users.noreply.github.com>
2023-03-15 23:13:53 +0530

    Add files via upload

---
+++ Sapling Swing Discussion/SwingLearning.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SwingLearning{
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Sapling Session");
+        frame.setBounds(200,200,500,500);
+        frame.getContentPane().setBackground(Color.green);
+        frame.setVisible(true);
+    }
+
+}
+
